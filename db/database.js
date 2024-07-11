3744321fdf3ae82d76b3da41d75cc304193b35a5
Saksham1904 <sakshamgoel1901@gmail.com>
2024-07-12 00:27:29 +0530

    changes made final

--- db/database.js
+++ db/database.js
@@ -2,9 +2,8 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 exports.connect = () => {
-    mongoose.connect(process.env.DATABASE_URL,{
-        useNewUrlParser:true,
-        useUnifiedTopology:true,
+    mongoose.connect("mongodb+srv://sakshamgoel1901:kl71wY92rPsyE5nv@saksham.ap8r7m0.mongodb.net/Medicine",{
+        
     }).then(()=>console.log("connection successfull"))
     .catch((error)=>{
         console.log("issue in db connection")
